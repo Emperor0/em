b15a77b4e6cf05f19f7a21f41f050d605d7ca9fb
Emperor0 <82902784+Emperor0@users.noreply.github.com>
2026-08-30 09:23:19 +0300

    Attach measured Network Lab to D7KT shell

--- D7SystemIntelligence/App.xaml.cs
+++ D7SystemIntelligence/App.xaml.cs
@@ -5,18 +5,22 @@ namespace D7SystemIntelligence;
 public partial class App : Application
 {
     private AppIntelligenceIntegration? _appIntelligence;
+    private NetworkLabIntegration? _networkLab;
 
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
         var window = new D7KtShellWindow();
         _appIntelligence = AppIntelligenceIntegration.Attach(window);
+        _networkLab = NetworkLabIntegration.Attach(window);
         MainWindow = window;
         window.Show();
     }
 
     protected override void OnExit(ExitEventArgs e)
     {
+        _networkLab?.Dispose();
+        _networkLab = null;
         _appIntelligence?.Dispose();
         _appIntelligence = null;
         base.OnExit(e);
