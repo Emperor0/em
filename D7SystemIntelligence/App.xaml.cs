d3d996f536ea8c6209f22bbc88824b990ecd19c8
Emperor0 <82902784+Emperor0@users.noreply.github.com>
2026-08-30 06:44:38 +0300

    Launch unified D7KT command center by default

--- D7SystemIntelligence/App.xaml.cs
+++ D7SystemIntelligence/App.xaml.cs
@@ -1,2 +1,14 @@
+using System.Windows;
+
 namespace D7SystemIntelligence;
-public partial class App : System.Windows.Application { }
+
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        var window = new D7KtShellWindow();
+        MainWindow = window;
+        window.Show();
+    }
+}
